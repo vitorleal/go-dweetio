dc5abaeb9e2c2f1a5e7f484da435205c7d57c7d3
Vitor Leal <vitorleal1@gmail.com>
2014-12-04 13:18:26 -0200

    initial commit

---
+++ dweetio.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+  "net/http"
+  "io/ioutil"
+  "encoding/json"
+  "fmt"
+)
+
+const (
+  BaseUrl = "https://dweet.io"
+)
+
+type Dweetio struct {
+	Key string
+}
+
+type Dweets struct {
+	This string
+	By string
+	The string
+	With []struct {
+		Thing string
+		Created string
+		Content map[string]interface{}
+	}
+}
+
+///Get last dweet for a Thing
+func (api *Dweetio) GetLastDweetFor(thing string) (dweets *Dweets, err error) {
+  uri := fmt.Sprintf("%s/get/latest/dweet/for/%s", BaseUrl, thing)
+  resp, err := http.Get(uri)
+
+  if err != nil {
+    return nil, err
+  }
+
+  defer resp.Body.Close()
+
+  //Read the response body
+  body, err := ioutil.ReadAll(resp.Body)
+
+  //If not error
+  if err != nil {
+    return nil, err
+  }
+
+  //Unmarshal the request body
+  if err := json.Unmarshal(body, &dweets); err != nil {
+	  return nil, err
+  }
+
+	return dweets, nil
+}
+
+///Get all dweet for a Thing
+func (api *Dweetio) GetDweetsFor(thing string) (dweets *Dweets, err error) {
+  uri := fmt.Sprintf("%s/get/dweets/for/%s", BaseUrl, thing)
+  resp, err := http.Get(uri)
+
+  if err != nil {
+    return nil, err
+  }
+
+  defer resp.Body.Close()
+
+  //Read the response body
+  body, err := ioutil.ReadAll(resp.Body)
+
+  //If not error
+  if err != nil {
+    return nil, err
+  }
+
+  //Unmarshal the request body
+  if err := json.Unmarshal(body, &dweets); err != nil {
+	  return nil, err
+  }
+
+	return dweets, nil
+}
+
+
+//Main func
+func main() {
+	api := Dweetio{}
+	res, err := api.GetDweetsFor("vitorleal")
+
+  if err != nil {
+    fmt.Println(err)
+	}
+
+	fmt.Println(res)
+}
