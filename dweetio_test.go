83b497c933d1845159270f6de03e0c60ac2d7be1
Vitor Leal <vitorleal1@gmail.com>
2014-12-08 20:17:07 -0200

    add simple test file

--- dweetio_test.go
+++ dweetio_test.go
@@ -1,3 +1,17 @@
 package dweetio
 
+import(
+  "testing"
+  "fmt"
+)
 
+func TestSet(t *testing.T) {
+	api := Dweetio{}
+	res, err := api.GetDweetsFor("vitorleal")
+
+	if err != nil {
+	  fmt.Println(err)
+  }
+
+	fmt.Println(res)
+}
