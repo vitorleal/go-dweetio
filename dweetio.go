43f89976f66b2483817cb7269544601f18b7ea67
Vitor Leal <vitorleal1@gmail.com>
2014-12-15 23:42:38 -0200

    add post method to the api, fix #1

--- dweetio.go
+++ dweetio.go
@@ -3,6 +3,7 @@ package dweetio
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 const (
@@ -14,11 +15,12 @@ func Version() string {
 	return "0.1.0"
 }
 
+//Dweetio struct
 type Dweetio struct {
 	Key string
 }
 
-//Get last dweet for a Thing
+//GET last dweet for a Thing
 func (api *Dweetio) GetLastDweetFor(thing string) (dweets interface{}, err error) {
 	uri := fmt.Sprintf("%s/get/latest/dweet/for/%s", BaseUrl, thing)
 	res, err := http.Get(uri)
@@ -34,7 +36,7 @@ func (api *Dweetio) GetLastDweetFor(thing string) (dweets interface{}, err error
 	return dweets, nil
 }
 
-//Get all dweet for a Thing
+//GET all dweet for a Thing
 func (api *Dweetio) GetDweetsFor(thing string) (dweets interface{}, err error) {
 	uri := fmt.Sprintf("%s/get/dweets/for/%s", BaseUrl, thing)
 	res, err := http.Get(uri)
@@ -48,4 +50,21 @@ func (api *Dweetio) GetDweetsFor(thing string) (dweets interface{}, err error) {
 	api.ReturnError(err)
 
 	return dweets, nil
+}
+
+//POST data to a Thing
+func (api *Dweetio) DweetFor(thing string, data string) (dweet interface{}, err error) {
+	uri := fmt.Sprintf("%s/dweet/for/%s", BaseUrl, thing)
+	json := strings.NewReader(data)
+
+	res, err := http.Post(uri, "application/json", json)
+
+	defer res.Body.Close()
+
+	dweet, err = api.ReadData(res)
+
+	//If error
+	api.ReturnError(err)
+
+	return dweet, nil
 }
