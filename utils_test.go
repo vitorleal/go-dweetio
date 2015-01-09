7e40fd797b3f431f343aad03e3d868ddd5d42438
Vitor Leal <vitorleal1@gmail.com>
2015-01-09 11:52:07 -0200

    add test for the GetAlertUri

--- utils_test.go
+++ utils_test.go
@@ -1,6 +1,7 @@
 package dweetio
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -15,5 +16,16 @@ func TestGetUri(t *testing.T) {
 
 	if uri2 != "https://dweet.io/get/dweets/for/godweetio" {
 		t.Error("Error in the GetUri")
+	}
+}
+
+func TestGetAlertUri(t *testing.T) {
+	api := Dweetio{}
+	uri := api.GetAlertUri("vitorleal1@gmail.com", "godweetio", "if(dweet.temp <= 32) return \"frozen\";")
+
+	fmt.Println(uri)
+
+	if uri != "https://dweet.io/alert/vitorleal1@gmail.com/when/godweetio/if%28dweet.temp+%3C%3D+32%29+return+%22frozen%22%3B" {
+		t.Error("Error in the GetAlertUri")
 	}
 }
